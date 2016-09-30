a3bafff007fe839a5e2e0f1375b33e30465b427c
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-09-30 14:35:16 +0900

    Change stylesheets to Sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var path = require('path');
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css')
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
@@ -13,101 +14,102 @@ var runSequence = require('run-sequence');
 
 
 var paths = {
-    source: 'source',
-    build: 'build',
-    html: '',
-    css: 'stylesheets',
-    js: 'javascripts',
-    font: 'fonts',
-    image: 'images',
+  source: 'source',
+  build: 'build',
+  html: '',
+  css: 'stylesheets',
+  js: 'javascripts',
+  font: 'fonts',
+  image: 'images',
 };
 
 var source = {
-    root: paths.source,
-    html: path.join(paths.source, paths.html),
-    css: path.join(paths.source, paths.css),
-    js: path.join(paths.source, paths.js),
-    font: path.join(paths.source, paths.font),
-    image: path.join(paths.source, paths.image)
+  root: paths.source,
+  html: path.join(paths.source, paths.html),
+  css: path.join(paths.source, paths.css),
+  js: path.join(paths.source, paths.js),
+  font: path.join(paths.source, paths.font),
+  image: path.join(paths.source, paths.image)
 };
 
 var build = {
-    root: paths.build,
-    html: path.join(paths.build, paths.html),
-    css: path.join(paths.build, paths.css),
-    js: path.join(paths.build, paths.js),
-    font: path.join(paths.build, paths.font),
-    image: path.join(paths.build, paths.image)
+  root: paths.build,
+  html: path.join(paths.build, paths.html),
+  css: path.join(paths.build, paths.css),
+  js: path.join(paths.build, paths.js),
+  font: path.join(paths.build, paths.font),
+  image: path.join(paths.build, paths.image)
 };
 
 var server = {
-    host: 'localhost',
-    port: '8000'
+  host: 'localhost',
+  port: '8000'
 };
 
 gulp.task('html', function(){
-    return gulp.src(path.join(source.html, '**/*.html'))
-	.pipe(gulp.dest(build.html));
+  return gulp.src(path.join(source.html, '**/*.html'))
+  .pipe(gulp.dest(build.html));
 });
 
 gulp.task('css', function(){
-    return gulp.src(path.join(source.css, '**/*.css'))
-	.pipe(concat('all.min.css'))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest(build.css));
+  return gulp.src(path.join(source.css, '**/*.scss'))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('all.min.css'))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest(build.css));
 });
 
 gulp.task('js', function(){
-    return gulp.src(path.join(source.js, '**/*.js'))
-	.pipe(uglify())
-	.pipe(concat('all.min.js'))
-	.pipe(gulp.dest(build.js));
+  return gulp.src(path.join(source.js, '**/*.js'))
+  .pipe(uglify())
+  .pipe(concat('all.min.js'))
+  .pipe(gulp.dest(build.js));
 });
 
 gulp.task('font', function(){
-    return gulp.src(path.join(source.font, '**/*'))
-	.pipe(gulp.dest(build.font));
+  return gulp.src(path.join(source.font, '**/*'))
+  .pipe(gulp.dest(build.font));
 });
 
 gulp.task('image', function(){
-    var glob = '**/*.+(jpg|jpeg|png|gif|svg)';
-    return gulp.src(path.join(source.image, glob))
-	.pipe(imagemin())
-	.pipe(gulp.dest(build.image));
+  var glob = '**/*.+(jpg|jpeg|png|gif|svg)';
+  return gulp.src(path.join(source.image, glob))
+  .pipe(imagemin())
+  .pipe(gulp.dest(build.image));
 });
 
 gulp.task('clean', function(callback){
-    return del([build.root], callback);
+  return del([build.root], callback);
 });
 
 gulp.task('webserver', function(){
-    gulp.src(build.root)
-	.pipe(webserver({
-	    host: server.host,
-	    port: server.port,
-	    livereload: true
-	}));
+  gulp.src(build.root)
+  .pipe(webserver({
+    host: server.host,
+    port: server.port,
+    livereload: true
+  }));
 });
 
 gulp.task('watch', function(){
-    gulp.watch(path.join(source.html, '**/*.html'), ['html']);
-    gulp.watch(path.join(source.css, '**/*.css'), ['css']);
-    gulp.watch(path.join(source.js, '**/*.js'), ['js']);
+  gulp.watch(path.join(source.html, '**/*.html'), ['html']);
+  gulp.watch(path.join(source.css, '**/*.scss'), ['css']);
+  gulp.watch(path.join(source.js, '**/*.js'), ['js']);
 });
 
 gulp.task('build', function(callback){
-    runSequence(
-	'clean',
-	['html', 'css', 'js', 'font', 'image'],
-	callback
-    );
+  runSequence(
+    'clean',
+    ['html', 'css', 'js', 'font', 'image'],
+    callback
+  );
 });
 
 gulp.task('start', function(callback) {
-    runSequence(
-	'build',
-	'webserver',
-	'watch',
-	callback
-    );
+  runSequence(
+    'build',
+    'webserver',
+    'watch',
+    callback
+  );
 });
